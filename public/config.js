72e91ac92677fe166c4f20f11b1c2939a9a75371
bbhsvip <bbhsvip@hotmail.com>
2025-03-01 19:57:36 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,19 +1,19 @@
 window.Config = {
 
   // 显示标题
-  SiteName: '寒舍网络空间监控',
+  SiteName: '寒舍网络空间监控站',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2848432-31a285b48d16eb00e84e37ad',
+    'ur2849306-50b49f2bf7f8b0ef737573c8',
   ],
 
   // 日志天数
-  CountDays: 30,
+  CountDays: 60,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 背景图片
   Image: 'https://ps.ssl.qhmsg.com/t0200ce14e175af6dcf.jpg',
@@ -25,7 +25,7 @@ window.Config = {
   Navi: [
     {
       text: '主页',
-      url: 'https://wwwq.us.kg'
+      url: 'https://wwqq.us.kg'
     },
     {
       text: 'Github',
